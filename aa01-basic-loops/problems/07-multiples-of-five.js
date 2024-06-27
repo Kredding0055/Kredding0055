ce015522d61492546b0b7aa92d629eb5bf3f4053
Kredding0055 <redkr3815@gmail.com>
2024-06-27 14:37:33 -0700

    Problem 7

--- aa01-basic-loops/problems/07-multiples-of-five.js
+++ aa01-basic-loops/problems/07-multiples-of-five.js
@@ -4,12 +4,22 @@ return a count of all numbers greater than or equal to 0 and less than the
 number parameter that are multiples of 5.
 */
 
-// Your code here 
+function multiplesOfFive(num) {
+    let i = '';
+    let count = 0;
+    for (let i = 0; i < num; i++) { 
+        if (i % 5 === 0) {
+            count += 1
+        }
 
-// console.log(multiplesOfFive(20)); // => 4    // 0, 5, 10, 15
-// console.log(multiplesOfFive(10)); // => 2    // 0, 5
-// console.log(multiplesOfFive(14)); // => 3    // 0, 5, 10
-// console.log(multiplesOfFive(21)); // => 5    // 0, 5, 10, 15, 20
+    }
+    return count;
+}
+
+ console.log(multiplesOfFive(20)); // => 4    // 0, 5, 10, 15
+ console.log(multiplesOfFive(10)); // => 2    // 0, 5
+ console.log(multiplesOfFive(14)); // => 3    // 0, 5, 10
+ console.log(multiplesOfFive(21)); // => 5    // 0, 5, 10, 15, 20
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = multiplesOfFive;
