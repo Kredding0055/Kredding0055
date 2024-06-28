7220869d8d50cf06d0f4d70325c90a6b30ae5362
Kredding0055 <redkr3815@gmail.com>
2024-06-27 19:16:15 -0700

    Problem 14

--- aa01-basic-loops/problems/14-reverse-string.js
+++ aa01-basic-loops/problems/14-reverse-string.js
@@ -4,13 +4,15 @@ return a new string where the order of the characters is reversed.
 */
 
 function reverseString(word) {
-    let i = '';
-    let index = 0;
-    for (i = word[index]; i > word.length -1; i--)
+    let newWord = '';
+    for (let i = word.length -1; i >= 0; i--) {
+        newWord += word[i];
+    }
+    return newWord;
 }
 
-// console.log(reverseString('fish')); // 'hsif'
-// console.log(reverseString('marathon')); // 'nohtaram'
-
+console.log(reverseString('fish')); // 'hsif'
+console.log(reverseString('marathon')); // 'nohtaram'
+console.log(reverseString('racecar'))
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = reverseString;
