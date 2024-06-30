419cbcaa2e74c79e5788a49ce28ff841c4415717
Kredding0055 <redkr3815@gmail.com>
2024-06-29 21:33:34 -0700

    Problem 25

--- aa01-basic-loops/problems/25-triplet-true.js
+++ aa01-basic-loops/problems/25-triplet-true.js
@@ -4,13 +4,26 @@ should return a boolean indicating whether or not the the string contains three
 of the same character consecutively.
 */
 
-// Your code here 
+function tripletTrue(str) {
+    if(str.length < 3) {
+        return false;
+    }
+    
+    for(let i = 2; i < str.length; i++) {
+        let letter = str[i];
+        if(letter == str[i-1] && letter == str[i-2]) {
+            return true;
+        }
 
-// console.log(tripletTrue('caaabb'));        // true
-// console.log(tripletTrue('terrrrrible'));   // true
-// console.log(tripletTrue('runninggg'));     // true
-// console.log(tripletTrue('bootcamp'));      // false
-// console.log(tripletTrue('e'));             // false
+    }
+    return false;
+}
+
+console.log(tripletTrue('caaabb'));        // true
+console.log(tripletTrue('terrrrrible'));   // true
+console.log(tripletTrue('runninggg'));     // true
+console.log(tripletTrue('bootcamp'));      // false
+console.log(tripletTrue('e'));             // false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = tripletTrue;
