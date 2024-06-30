16045839a5d8607b47d865ae0baabd8f8aaefc82
Kredding0055 <redkr3815@gmail.com>
2024-06-29 21:44:44 -0700

    Problem 26

--- aa01-basic-loops/problems/26-more-dot-less-dash.js
+++ aa01-basic-loops/problems/26-more-dot-less-dash.js
@@ -4,13 +4,31 @@ function should return a boolean indicating whether or not the string contains
 more dots (.) than dashes (-).
 */
 
-// Your code here 
+function moreDotLessDash(str) {
+    dash = 0;
+    dot = 0; 
+    for(i = 0; i < str.length; i++) {
+        letter = str[i];
+        if(letter == '-') {
+            dash +=1;
+        }
+        if(letter == '.') {
+            dot +=1;
+        }
+        
+    }
+    if(dot > dash) {
+        return true;
+    }
+    return false;
+}
 
-// console.log(moreDotLessDash('2-D arrays are fun. I think.'));           // true
-// console.log(moreDotLessDash('Morse code is great.'));                   // true
-// console.log(moreDotLessDash('.... . -.--'));                            // true
-// console.log(moreDotLessDash('.--. .-. --- --. .-. .- -- -- . .-.'));    // false
-// console.log(moreDotLessDash('high-flying acrobat.'));                   // false
+
+console.log(moreDotLessDash('2-D arrays are fun. I think.'));           // true
+console.log(moreDotLessDash('Morse code is great.'));                   // true
+console.log(moreDotLessDash('.... . -.--'));                            // true
+console.log(moreDotLessDash('.--. .-. --- --. .-. .- -- -- . .-.'));    // false
+console.log(moreDotLessDash('high-flying acrobat.'));                   // false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = moreDotLessDash;
