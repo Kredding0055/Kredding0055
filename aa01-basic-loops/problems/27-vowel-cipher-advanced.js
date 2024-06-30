f3caf42beaec6bc63f1d9aa95765b5a2ee0cfbdf
Kredding0055 <redkr3815@gmail.com>
2024-06-29 22:19:27 -0700

    Problem 27

--- aa01-basic-loops/problems/27-vowel-cipher-advanced.js
+++ aa01-basic-loops/problems/27-vowel-cipher-advanced.js
@@ -5,10 +5,25 @@ where every vowel becomes the next vowel in the alphabet.
 Vowels are the letters "a", "e", "i", "o", "u".
 */
 
-// Your code here 
+function vowelCipher(str) {
+    let vowel = 'aeiou';
+    let str1 = '';
+    for(let i = 0; i < str.length; i++) {
+        let letter = str[i];
+        let vowelIndex = vowel.indexOf(letter);
+        if(vowelIndex >= 0) {
+          modVowel = (vowelIndex+1) % vowel.length;
+          str1 = str1 + vowel[modVowel];
+        }
+        else {
+            str1 = str1 + letter;
+        }
+    }
+return str1;
+}
 
-// console.log(vowelCipher("bootcamp")); // "buutcemp"
-// console.log(vowelCipher("paper cup")); // "pepir cap"
+console.log(vowelCipher("bootcamp")); // "buutcemp"
+console.log(vowelCipher("paper cup")); // "pepir cap"
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = vowelCipher;
