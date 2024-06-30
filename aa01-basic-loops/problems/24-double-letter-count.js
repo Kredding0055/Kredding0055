658ea7ba31ec750601553341488cf94a1e65ad2f
Kredding0055 <redkr3815@gmail.com>
2024-06-29 21:23:17 -0700

    Probelm 24

--- aa01-basic-loops/problems/24-double-letter-count.js
+++ aa01-basic-loops/problems/24-double-letter-count.js
@@ -3,10 +3,20 @@ Write a function doubleLetterCount that takes in a string and returns the number
 of times that the same letter repeats twice in a row.
 */
 
-// Your code here 
+function doubleLetterCount(str) {
+    let count = 0; 
 
-// console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
-// console.log(doubleLetterCount("bootcamp")); // 1
+    for(let i = 1; i < str.length; i++) {
+        let letter = str[i];
+        if(letter == str[i -1]) {
+        count += 1;
+        }
+    }
+    return count;
+}
+
+console.log(doubleLetterCount("the jeep rolled down the hill"));  // 3
+console.log(doubleLetterCount("bootcamp")); // 1
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = doubleLetterCount;
