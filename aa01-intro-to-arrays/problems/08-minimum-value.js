9bdc24bf2ef931675473822a006e977ced492b13
Kredding0055 <redkr3815@gmail.com>
2024-07-01 13:11:15 -0700

    Problem 8

--- aa01-intro-to-arrays/problems/08-minimum-value.js
+++ aa01-intro-to-arrays/problems/08-minimum-value.js
@@ -4,12 +4,28 @@ parameter. The function should return the smallest number of the array. If the
 array is empty, the function should return null.
 */
 
-// Your code here 
+function minVal(array) {
+    let num = array[0];
+
+    if(array.length === 0) {
+        return null;
+    }
+    for(let i = 0; i < array.length; i++) {
+        let num2 = array[i +1];
+        if(num < num2) {
+            num = num;
+        }
+        if(num > num2) {
+            num = num2;
+        }
+    }
+    return num;
+} 
 
 
-// console.log(minVal([4, 6, 3, 5, 2, 4])); // 2
-// console.log(minVal([-2, -3, -7, 3 ]));   // -7
-// console.log(minVal([])); //null
+console.log(minVal([4, 6, 3, 5, 2, 4])); // 2
+console.log(minVal([-2, -3, -7, 3 ]));   // -7
+console.log(minVal([])); //null
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = minVal;
