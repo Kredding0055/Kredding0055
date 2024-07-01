a90549e194f43d1ee600ceee87c7cdb296cbb752
Kredding0055 <redkr3815@gmail.com>
2024-07-01 14:07:57 -0700

    Problem 10

--- aa01-intro-to-arrays/problems/10-has-element.js
+++ aa01-intro-to-arrays/problems/10-has-element.js
@@ -5,15 +5,30 @@ return a boolean indicating whether the target element is in the array.
 Solve this using a loop.
 */
 
-// Your code here 
+function hasElement(char1, char2) {
+
+    if(char1.length === 0 || char2.length === 0) {
+        return false;
+    }
+
+    for(let i = 0; i < char1.length; i ++) {
+        if((char2[0]) === (char1[i])) {
+            return true;
+        }
+        if(char2 == char1[i]) {
+            return true;
+        }
+    }
+    return false;
+}
 
 
-// console.log(hasElement(['a', 'b', 'c', 'e'], 'c')); // true
-// console.log(hasElement(['a', 'b', 'c', 'e'], 'a')); // true
-// console.log(hasElement(['a', 'b', 'c', 'e'], 'z')); // false
-// console.log(hasElement([43, -7, 11, 13], 11));      // true
-// console.log(hasElement([43, -7, 11, 13], 1));       // false
-// console.log(hasElement([], 3));                     // false
+console.log(hasElement(['a', 'b', 'c', 'e'], 'c')); // true
+console.log(hasElement(['a', 'b', 'c', 'e'], 'a')); // true
+console.log(hasElement(['a', 'b', 'c', 'e'], 'z')); // false
+console.log(hasElement([43, -7, 11, 13], 11));      // true
+console.log(hasElement([43, -7, 11, 13], 1));       // false
+console.log(hasElement([], 3));                     // false
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = hasElement;
