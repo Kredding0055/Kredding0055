76652e522bba5a3b0874ab4b5b5f1ff887d424b3
Kredding0055 <redkr3815@gmail.com>
2024-07-01 09:51:07 -0700

    Problem 2

--- aa01-intro-to-arrays/problems/02-first-and-last.js
+++ aa01-intro-to-arrays/problems/02-first-and-last.js
@@ -5,14 +5,30 @@ array. If there is an odd number of elements in the array, then the function
 should return the difference between the first and last elements of the array.
 */
 
-// Your code here 
+function firstAndLast(array) {
+
+    if(array.length <= 1) {
+        return 0;
+    }
+    if(array.length % 2 === 0) {
+        last = array[array.length -1];
+        str = array[0] + last;
+        return str;
+    }
+    if(array.length % 2 === 1) {
+        last = array[array.length -1];
+        str = array[0] - last;
+        return str;
+    }
+
+}
 
 
-// console.log(firstAndLast([1, 2, 3, 4]));    // 5
-// console.log(firstAndLast([1, 2, 3, 4, 5])); // -4
-// console.log(firstAndLast([12, 5]));         // 17
-// console.log(firstAndLast([12]));            // 0
-// console.log(firstAndLast([7, 11, 3]));      // 4
+console.log(firstAndLast([1, 2, 3, 4]));    // 5
+console.log(firstAndLast([1, 2, 3, 4, 5])); // -4
+console.log(firstAndLast([12, 5]));         // 17
+console.log(firstAndLast([12]));            // 0
+console.log(firstAndLast([7, 11, 3]));      // 4
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = firstAndLast;
