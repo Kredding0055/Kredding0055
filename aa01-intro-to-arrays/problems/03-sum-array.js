439acce9cbee6781ab4693f0bafbe9f0b1040a74
Kredding0055 <redkr3815@gmail.com>
2024-07-01 10:15:56 -0700

    Problem 3

--- aa01-intro-to-arrays/problems/03-sum-array.js
+++ aa01-intro-to-arrays/problems/03-sum-array.js
@@ -2,13 +2,21 @@
 returns the total sum of all the numbers.
 */
 
-// Your code here 
+function sumArray(array) {
+    let count = 0;
+
+    for(let i = 0; i < array.length; i++) {
+        count = count + array[i];
+        
+    }
+    return count;
+} 
 
 
-// console.log(sumArray([1, 2]));        // => 3
-// console.log(sumArray([5, 6, 4]));     // => 15
-// console.log(sumArray([7, 3, 9, 11])); // => 30
-// console.log(sumArray([]));            // => 0
+console.log(sumArray([1, 2]));        // => 3
+console.log(sumArray([5, 6, 4]));     // => 15
+console.log(sumArray([7, 3, 9, 11])); // => 30
+console.log(sumArray([]));            // => 0
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = sumArray;
