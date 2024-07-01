d8db6bfe5cb142527475f260781c76eaea7fa375
Kredding0055 <redkr3815@gmail.com>
2024-07-01 13:13:19 -0700

    Problem 9

--- aa01-intro-to-arrays/problems/09-maximum-value.js
+++ aa01-intro-to-arrays/problems/09-maximum-value.js
@@ -4,12 +4,28 @@ parameter. The function should return the largest number of the array. If the
 array is empty, the function should return null.
 */
 
-// Your code here 
+function maxVal(array) {
+    let num = array[0];
+
+    if(array.length === 0) {
+        return null;
+    }
+    for(let i = 0; i < array.length; i++) {
+        let num2 = array[i +1];
+        if(num > num2) {
+            num = num;
+        }
+        if(num < num2) {
+            num = num2;
+        }
+    }
+    return num;
+}
 
 
-// console.log(maxVal([4, 6, 3, 5, 42, 4])); // 42
-// console.log(maxVal([-2, -3, -7, 3 ]));    // 3
-// console.log(maxVal([]));                  // null
+console.log(maxVal([4, 6, 3, 5, 42, 4])); // 42
+console.log(maxVal([-2, -3, -7, 3 ]));    // 3
+console.log(maxVal([]));                  // null
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = maxVal;
