7363a285a6275cc05ab9840af85f234e217c270f
Kredding0055 <redkr3815@gmail.com>
2024-07-11 10:55:53 -0700

    01

---
+++ aa02-intro-to-closure/problems/01-sundae-order.js
@@ -0,0 +1,34 @@
+/*
+Implement the `sundaeOrder()` function. This function should return another
+function that accepts a string (`topping`) to add to the sundae. 
+A sundae will start off with a default "A bowl of ice cream with hot fudge".
+Each incoming topping should be separated by "and". The function skeleton has
+already been created for you - your job is to fill in the missing logic.
+
+Look at the example test cases to see how this function is invoked:
+*/
+
+function sundaeOrder() {
+  let sundae = 'A bowl of ice cream with hot fudge';
+
+  return function (topping) {
+    // console.log('nuts');
+    // console.log(sundae);
+    sundae += ' and ' + topping;
+    return sundae
+  };
+}
+
+const sundae1 = sundaeOrder(); // => returns a function
+console.log(sundae1("nuts")) // => "A bowl of ice cream with hot fudge and nuts"
+console.log(sundae1("caramel")) // => "A bowl of ice cream with hot fudge and nuts and caramel"
+
+const sundae2 = sundaeOrder(); // => returns a function
+console.log(sundae2("banana")) // => "A bowl of ice cream with hot fudge and banana"
+
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+
+try {
+  module.exports = sundaeOrder;
+} catch {}
